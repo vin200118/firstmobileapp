466b7570e9ccae4750cec972ae7e9b976fa48d84
vin200118 <padekarvinayak5@gmail.com>
2019-10-13 18:59:35 +0530

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
    entry: './index.js',
    output: {
-	    path: path.resolve(__dirname, __dirname),
+	    path: path.resolve(__dirname, "builder"),
 	    filename: 'index_bundle.js',
 	    publicPath: '/'
 	  },
+	  
+	  optimization: {
+		    splitChunks: {
+		      chunks: 'async',
+		      minSize: 30000,
+		      maxSize: 0,
+		      minChunks: 1,
+		      maxAsyncRequests: 5,
+		      maxInitialRequests: 3,
+		      automaticNameDelimiter: '~',
+		      automaticNameMaxLength: 30,
+		      name: true,
+		      cacheGroups: {
+		        vendors: {
+		          test: /[\\/]node_modules[\\/]/,
+		          priority: -10
+		        },
+		        default: {
+		          minChunks: 2,
+		          priority: -20,
+		          reuseExistingChunk: true
+		        }
+		      }
+		    }
+		  },  
+	  
+	  
    devServer: {
       inline: true,
       port: 8080,
