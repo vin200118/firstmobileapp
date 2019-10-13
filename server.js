d95e37131298597a71cc206af541c76897ae044d
vin200118 <padekarvinayak5@gmail.com>
2019-10-13 19:39:15 +0530

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static("/builder"));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve("/builder", 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
